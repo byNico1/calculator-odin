1b2939b837d5d17c9964ae47cde120dbfb3db950
bynico1 <nicofoque0205@gmail.com>
2023-08-01 17:17:28 -0500

    first files and added style to the calculator

---
+++ main.js
@@ -0,0 +1,10 @@
+const add = (a, b) => a + b;
+const substract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+const operate = (fNumber, operator, sNumber) => {};
